71d3d217be6dcabc6c2c4dd11bca9988c9cfe679
dallaer <106924385+dallaer@users.noreply.github.com>
2022-06-05 20:22:08 +0300

    Add files via upload

---
+++ llibrary/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.urls import include
+from django.contrib import admin
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
